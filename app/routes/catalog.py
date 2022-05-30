c45d54339f7a0f415281a82ed6aec2f3200e1eb4
SageTendo <nz.zishiri@gmail.com>
2022-05-30 14:44:43 +0200

    Meta handler implementation and some code refactoring.

--- app/routes/catalog.py
+++ app/routes/catalog.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request
 from werkzeug.exceptions import abort
 
 from . import MANIFEST, mal_client
-from .utils import respond_with
+from .utils import respond_with, mal_to_meta
 
 catalog_bp = Blueprint('catalog', __name__)
 
@@ -29,31 +29,11 @@ def addon_catalog(token: str, catalog_type: str, catalog_id: str, offset: str = 
     response_data = mal_client.get_user_anime_list(token, status=catalog_id, offset=offset, fields=field_params)
     response_data = response_data['data']  # Get array of node objects
 
-    meta_previews = {'metas': []}
+    meta_previews = []
     for data_item in response_data:
         anime_item = data_item['node']
 
         # Metadata stuff
-        posters = anime_item['main_picture']
-        medium_poster = posters['medium']
-        # large_poster = posters['large']
-        genres = [genre['name'] for genre in anime_item['genres']]
-        rating = anime_item['mean'] if 'mean' in anime_item.keys() else 0
-        start_date = anime_item['start_date'][:4]  # Get the year only
-        description = anime_item['synopsis']
-
-        # Format start date if end_date of airing was returned
-        if 'end_date' not in anime_item.keys():
-            start_date += '-'
-
-        meta_previews['metas'].append({
-            'id': f"tt{anime_item['id']}",
-            'type': catalog_type,
-            'name': anime_item['title'],
-            'genres': genres,
-            'poster': medium_poster,
-            'imdbRating': rating,
-            'releaseInfo': start_date,
-            'description': description
-        })
-    return respond_with(meta_previews)
+        meta = mal_to_meta(anime_item, catalog_type)
+        meta_previews.append(meta)
+    return respond_with({'metas': meta_previews})
