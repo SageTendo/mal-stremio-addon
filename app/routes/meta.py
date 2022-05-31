c7349c7d164b9c10fa80bb399fd429cc4a7e9f8f
SageTendo <nz.zishiri@gmail.com>
2022-05-31 05:19:50 +0200

    Meta now includes an IMDB id that is fetched using Cinemagoer (IMDBPY) Code refactoring

--- app/routes/meta.py
+++ app/routes/meta.py
@@ -1,10 +1,11 @@
-import random
-
+import imdb
 from flask import Blueprint, abort
 from . import mal_client, MANIFEST
 from .utils import mal_to_meta, respond_with
 
 meta_bp = Blueprint('meta', __name__)
+
+imdb_client = imdb.Cinemagoer(accessSystem='http')
 
 
 @meta_bp.route('/<token>/meta/<meta_type>/<meta_id>.json')
@@ -15,13 +16,17 @@ def addon_meta(token: str, meta_type: str, meta_id: str):
 
     # Fetch anime details from MAL
     anime_id = meta_id.replace('mal-', '')  # Extract anime id from addon meta id
-    field_params = 'genres mean start_date end_date synopsis pictures'  # Additional fields to return
-
+    field_params = 'media_type genres mean start_date end_date synopsis pictures'  # Additional fields to return
     anime_item = mal_client.get_anime_details(token, anime_id, fields=field_params)
 
-    # Format the details to a meta format
-    anime_item = mal_to_meta(anime_item, meta_type)
-    # TODO
+    if anime_item:
+        # Format the details to a meta format
+        anime_item = mal_to_meta(anime_item, meta_type)
 
-    # Return with CORS to client
-    return respond_with({'meta': anime_item})
+        # Search for anime on IMDB
+        imdb_items = imdb_client.search_movie_advanced(title=anime_item['name'], adult=True, results=5)
+
+        # Add IMDB id to meta item
+        if imdb_items:
+            anime_item['imdb_id'] = f"tt{imdb_items[0].getID()}"
+    return respond_with({'meta': anime_item})  # Return with CORS to client
--- app/routes/utils.py
+++ app/routes/utils.py
@@ -13,17 +13,40 @@ def respond_with(data):
 
 def mal_to_meta(anime_item: dict, content_type: str):
     # Metadata stuff
-    content_id = f"mal-{anime_item['id']}"
-    poster_objects = anime_item['main_picture']
-    poster = poster_objects['medium']
-    # poster = poster_objects['large']
-    genres = [genre['name'] for genre in anime_item['genres']]
-    rating = anime_item['mean'] if 'mean' in anime_item.keys() else 0
-    start_date = anime_item['start_date'][:4]  # Get the year only
-    description = anime_item['synopsis']
+    content_id = f"mal-{anime_item['id']}"  # Format id to mal addon format
+
+    title = None
+    if 'title' in anime_item.keys():
+        title = anime_item['title']
+
+    poster = None
+    if 'main_picture' in anime_item.keys():
+        poster_objects = anime_item['main_picture']
+        poster = poster_objects['medium']
+        # poster = poster_objects['large']
+
+    genres = None
+    if 'genres' in anime_item.keys():
+        genres = [genre['name'] for genre in anime_item['genres']]
+
+    mean_score = None
+    if 'mean' in anime_item.keys():
+        mean_score = anime_item['mean']
+
+    synopsis = None
+    if 'synopsis' in anime_item.keys():
+        synopsis = anime_item['synopsis']
+
+    start_date = None
+    if 'start_date' in anime_item.keys():
+        start_date = anime_item['start_date'][:4]  # Get the year only or None
+
+        # Format start date if end_date of airing was not returned
+        if 'end_date' not in anime_item.keys():
+            start_date += '-'
 
     # Check for background key in anime_item
-    background = ''
+    background = None
     if 'pictures' in anime_item.keys():
         # Get the first picture from the list of picture objects
         # index = 0
@@ -34,18 +57,14 @@ def mal_to_meta(anime_item: dict, content_type: str):
         # Get the randomly chosen picture object's largest size
         background = anime_item['pictures'][index]['large']
 
-    # Format start date if end_date of airing was returned
-    if 'end_date' not in anime_item.keys():
-        start_date += '-'
-
     return {
         'id': content_id,
+        'name': title,
         'type': content_type,
-        'name': anime_item['title'],
         'genres': genres,
         'poster': poster,
         'background': background,
-        'imdbRating': rating,
+        'imdbRating': mean_score,
         'releaseInfo': start_date,
-        'description': description
+        'description': synopsis
     }
