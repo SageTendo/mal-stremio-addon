585852419e9e44525b4328275d12c55c42b1df55
SageTendo <nz.zishiri@gmail.com>
2022-06-01 22:26:52 +0200

    Implementation of stream handler

---
+++ app/routes/stream.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, abort
+from . import MANIFEST
+from .utils import respond_with
+
+stream_bp = Blueprint('stream', __name__)
+
+torrentio_API = "https://torrentio.strem.fun/stream/"
+torrentio_lite_API = "https://torrentio.strem.fun/lite/stream/"
+
+
+@stream_bp.route('/<token>/stream/<stream_type>/<content_id>.json')
+def addon_stream(token: str, stream_type: str, content_id: str):
+    streams = []
+    if stream_type not in MANIFEST['types']:
+        abort(404)
+
+    return respond_with({'stream': streams})
--- run.py
+++ run.py
@@ -1,12 +1,10 @@
-from dotenv import load_dotenv
 from flask import Flask, render_template
 
 from app.routes.auth import auth_blueprint
 from app.routes.catalog import catalog_bp
 from app.routes.manifest import manifest_blueprint
 from app.routes.meta import meta_bp
-
-load_dotenv()
+from app.routes.stream import stream_bp
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
 app.config.from_object('config.Config')
@@ -14,6 +12,7 @@ app.register_blueprint(manifest_blueprint)
 app.register_blueprint(catalog_bp)
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(meta_bp)
+app.register_blueprint(stream_bp)
 
 
 @app.route('/')
