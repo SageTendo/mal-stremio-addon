cb58c51ce3c1a12af1f647cbcd7580d36be3f559
SageTendo <nz.zishiri@gmail.com>
2022-06-04 19:05:32 +0200

    Manifest Handler

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,29 +1,5 @@
-from app.api import LIST_STATUS
 from app.api.mal import MyAnimeListAPI
 
 mal_client = MyAnimeListAPI()
-
-MANIFEST = {
-    'id': 'com.sagetendo.mal-stremio-addon',
-    'version': '1.0.0',
-
-    'name': 'MAL Addon',
-    'description': 'MyAnimeList watchlist addon',
-
-    'types': ['anime', 'series', 'movie'],
-
-    'catalogs': [
-        {
-            'type': 'anime',
-            'id': f'{status}',
-            'name': f'MAL {status.replace("_", " ").capitalize()}',
-            'extra': [{'name': 'search'}]
-        } for status in LIST_STATUS
-    ],
-
-    'resources': [
-        'catalog', 'meta', 'stream'
-    ],
-
-    'idPrefixes': ['mal-', 'tt', 'kitsu:']
-}
+MAL_ID_PREFIX = "mal:"
+IMDB_ID_PREFIX = "tt"
