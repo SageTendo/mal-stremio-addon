1a86b8d93d64073eba5aa137bfebd0e177277594
SageTendo <nz.zishiri@gmail.com>
2022-06-13 21:05:50 +0200

    Code refactor

--- app/routes/meta.py
+++ app/routes/meta.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+
 import requests
 from flask import Blueprint, abort
 from . import mal_client, MAL_ID_PREFIX
@@ -5,13 +8,13 @@ from .manifest import MANIFEST
 from .utils import mal_to_meta, respond_with
 from ..db.db import map_db
 
-meta_bp = Blueprint('meta', __name__)
+meta = Blueprint('meta', __name__)
 
 # Where we fetch the kitsu id of an anime based on the IMDB id. episodes based on
 kitsu_API = "https://anime-kitsu.strem.fun/meta"
 
 
-@meta_bp.route('/<token>/meta/<meta_type>/<meta_id>.json')
+@meta.route('/<token>/meta/<meta_type>/<meta_id>.json')
 def addon_meta(token: str, meta_type: str, meta_id: str):
     # Check if meta type exists in manifest
     if meta_type not in MANIFEST['types']:
@@ -31,27 +34,31 @@ def addon_meta(token: str, meta_type: str, meta_id: str):
 
         # Add IMDB id to meta item
         if anime_mapping:
-            kitsu_id = None
-            if 'kitsu_id' in anime_mapping.keys():
-                kitsu_id = f"kitsu:{anime_mapping['kitsu_id']}"
+            kitsu_id = f'kitsu:{anime_mapping.get("kitsu_id")}'
+            if kitsu_id:
                 anime_item['kistu_id'] = kitsu_id
 
             # Call Kitsu Addon for Kitsu metadata
-            anime_media_type = anime_item['type'] or 'anime'
+            anime_media_type = anime_item.get('type', 'anime')
             resp = requests.get(f'{kitsu_API}/{anime_media_type}/{kitsu_id}.json')
 
             if 200 <= resp.status_code <= 299:
-                kitsu_meta = resp.json()['meta']
+                kitsu_meta = resp.json().get('meta')
 
                 # Replace mal id with kitsu id
-                if 'imdb_id' in kitsu_meta.keys():
-                    anime_item['imdb_id'] = kitsu_meta['imdb_id']
+                imdb_id = kitsu_meta.get('imdb_id')
+                if imdb_id:
+                    anime_item['imdb_id'] = imdb_id
 
                 # Check for logo and add it to meta
-                if 'logo' in kitsu_meta.keys():
-                    anime_item['logo'] = kitsu_meta['logo']
+                logo = kitsu_meta.get('logo')
+                if logo:
+                    anime_item['logo'] = logo
 
                 # Add videos to kitsu if they exist
-                if 'videos' in kitsu_meta.keys():
-                    anime_item['videos'] = kitsu_meta['videos']
+                videos = kitsu_meta.get('videos')
+                if videos:
+                    anime_item['videos'] = videos
+            else:
+                print("FETCH ERROR: Failed to call Kitsu API")
     return respond_with({'meta': anime_item})  # Return with CORS to client
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 from app.routes.auth import auth_blueprint
 from app.routes.catalog import catalog_bp
 from app.routes.manifest import manifest_blueprint
-from app.routes.meta import meta_bp
+from app.routes.meta import meta
 from app.routes.stream import stream_bp
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
@@ -11,7 +11,7 @@ app.config.from_object('config.Config')
 app.register_blueprint(manifest_blueprint)
 app.register_blueprint(catalog_bp)
 app.register_blueprint(auth_blueprint)
-app.register_blueprint(meta_bp)
+app.register_blueprint(meta)
 app.register_blueprint(stream_bp)
 
 
