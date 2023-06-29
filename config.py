c2ca3e539d474f4b993427574e6dde5c665c848e
nz.zishiri@gmail.com <nz.zishiri@gmail.com>
2023-06-29 17:31:06 +0200

    - environment variable can return string value of 1/0 or True/False

--- config.py
+++ config.py
@@ -12,14 +12,14 @@ class Config:
     JSON_SORT_KEYS = False
     FLASK_HOST = os.getenv('FLASK_RUN_HOST', "localhost")
     FLASK_PORT = os.getenv('FLASK_RUN_PORT', "5000")
-    DEBUG = os.getenv('FLASK_DEBUG', 0)
+    DEBUG = os.getenv('FLASK_DEBUG', False)
 
     MONGO_URI = os.getenv('MONGO_URI', "")
     MONGO_DB = os.getenv('MONGO_DB', "")
     MONGO_ANIME_MAP = os.getenv('MONGO_ANIME_MAP_COLLECTION', "")
 
     # redirect URI depending on environment
-    if int(DEBUG) == 1:
+    if DEBUG in ["1", True, "True"]:
         # Local development
         REDIRECT_URI = f"http://{FLASK_HOST}:{FLASK_PORT}"
     else:
