3b09175295b9c78a5cec0df892196f87b945f382
nz.zishiri@gmail.com <nz.zishiri@gmail.com>
2023-07-05 04:09:13 +0200

    - Refactor and code clean up

--- app/routes/meta.py
+++ app/routes/meta.py
@@ -28,42 +28,52 @@ def addon_meta(token: str, meta_type: str, meta_id: str):
     # Fetch anime details from MAL
     anime_id = meta_id.replace(MAL_ID_PREFIX, '')  # Extract anime id from addon meta id
     field_params = 'media_type genres mean start_date end_date synopsis pictures'  # Additional fields to return
-    anime_item = mal_client.get_anime_details(token, anime_id, fields=field_params)
+    anime_details = mal_client.get_anime_details(token, anime_id, fields=field_params)
 
-    if anime_item:
-        # Format the details to a meta format
-        anime_item = mal_to_meta(anime_item)
+    # Check if anime details exist
+    if not anime_details:
+        return respond_with({'meta': None})
 
-        # Fetch kitsu id from map db
-        anime_mapping = anime_map_collection.find_one({'mal_id': int(anime_id)})
+    # Format the details to stremio's meta format
+    anime_details = mal_to_meta(anime_details)
 
-        # Add IMDB id to meta item
-        if anime_mapping:
-            kitsu_id = f'kitsu:{anime_mapping.get("kitsu_id")}'
-            if kitsu_id:
-                anime_item['kitsu_id'] = kitsu_id
+    # Fetch kitsu id from map db
+    anime_mapping = anime_map_collection.find_one({'mal_id': int(anime_id)})
 
-            # Call Kitsu Addon for Kitsu metadata
-            anime_media_type = anime_item.get('type', 'anime')
-            resp = requests.get(f'{kitsu_API}/{anime_media_type}/{kitsu_id}.json')
+    # Add kitsu metadata to anime details
+    if anime_mapping:
+        if (kitsu_id := anime_mapping.get('kitsu_id', None)) is None:
+            return respond_with({'meta': anime_details})
 
-            if 200 <= resp.status_code <= 299:
-                kitsu_meta = resp.json().get('meta')
+        # Format kitsu id and add to meta
+        formated_kitsu_id = f'kitsu:{kitsu_id}'
+        anime_details['kitsu_id'] = formated_kitsu_id
 
-                # Replace mal id with kitsu id
-                imdb_id = kitsu_meta.get('imdb_id')
-                if imdb_id:
-                    anime_item['imdb_id'] = imdb_id
+        # Call Kitsu Addon for Kitsu metadata
+        anime_media_type = anime_details.get('type', 'anime')
+        resp = requests.get(f'{kitsu_API}/{anime_media_type}/{formated_kitsu_id}.json')
+        if 200 <= resp.status_code <= 299:
+            kitsu_meta = resp.json().get('meta')
 
-                # Check for logo and add it to meta
-                logo = kitsu_meta.get('logo')
-                if logo:
-                    anime_item['logo'] = logo
+            # add imdb id to meta
+            imdb_id = kitsu_meta.get('imdb_id')
+            if imdb_id:
+                anime_details['imdb_id'] = imdb_id
 
-                # Add videos to kitsu if they exist
-                videos = kitsu_meta.get('videos')
-                if videos:
-                    anime_item['videos'] = videos
-            else:
-                print("FETCH ERROR: Failed to call Kitsu API")
-    return respond_with({'meta': anime_item})  # Return with CORS to client
+            # Check for logo and add it to meta
+            logo = kitsu_meta.get('logo')
+            if logo:
+                anime_details['logo'] = logo
+
+            # Add videos to kitsu if they exist
+            videos = kitsu_meta.get('videos')
+            if videos:
+                anime_details['videos'] = videos
+
+            # Add links to kitsu if they exist
+            links = kitsu_meta.get('links')
+            if links:
+                anime_details['links'] = links
+        else:
+            print("FETCH ERROR: Failed to call Kitsu API")
+    return respond_with({'meta': anime_details})  # Return with CORS to client
