428a494ad54cd178edf10625619777895ade460d
nz.zishiri@gmail.com <nz.zishiri@gmail.com>
2024-02-09 19:30:27 +0200

    v1.2.0 Ignore imdb ids provided by older addon versions

--- app/routes/meta.py
+++ app/routes/meta.py
@@ -3,6 +3,7 @@ import logging
 import requests
 from flask import Blueprint, abort
 
+from . import IMDB_ID_PREFIX
 from .manifest import MANIFEST
 from .utils import respond_with
 
@@ -20,6 +21,10 @@ def addon_meta(user_id: str, meta_type: str, meta_id: str):
     :param meta_id: The ID of the content
     :return: JSON response
     """
+    # ignore imdb ids for older versions of mal-stremio
+    if IMDB_ID_PREFIX in meta_id:
+        return {}
+
     # Check if meta type exists in manifest
     if meta_type not in MANIFEST['types']:
         abort(404)
