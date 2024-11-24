6768e0c2abd0c33b8139bec463e255f5d56c6e2b
Nyasha Zishiri <23294558@sun.ac.za>
2024-11-24 22:32:59 +0200

    revert to flask-compress 1.14

--- run.py
+++ run.py
@@ -21,11 +21,6 @@ app.register_blueprint(content_sync_bp)
 app.register_blueprint(stream_bp)
 
 Compress(app)
-app.config['COMPRESS_MIMETYPES'] = [
-    'text/html',
-    'application/json'
-]
-app.config['COMPRESS_ALGORITHM'] = 'gzip'
 
 logging.basicConfig(format='%(asctime)s %(message)s')
 
