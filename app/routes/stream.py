a95d8128279c87514a474a26c8a84a911f9b1675
Nyasha Zishiri <23294558@sun.ac.za>
2025-01-30 17:04:32 +0200

    - Returning more streams - Using sync requests to allow caching streams - Ensure user has a valid token prevent making extra calls (this is to assume they aren't using the addon directly)

--- app/routes/stream.py
+++ app/routes/stream.py
@@ -4,22 +4,24 @@ import urllib.parse
 import httpx
 from flask import Blueprint
 
+import config
 from app.db.db import get_kitsu_id_from_mal_id
 from app.routes import MAL_ID_PREFIX, IMDB_ID_PREFIX
+from app.routes.auth import get_token
 from app.routes.manifest import MANIFEST
 from app.routes.utils import respond_with
 
 stream_bp = Blueprint('stream', __name__)
 
-limit = 5
+limit = 15
 providers = ("yts,eztv,rarbg,1337x,thepiratebay,kickasstorrents,"
              "torrentgalaxy,magnetdl,horriblesubs,nyaasi,tokyotosho,anidex")
-quality_filters = "brremux,hdrall,dolbyvision"
+quality_filters = "3Dbrremux,hdrall,dolbyvision,dolbyvisionwithhdr,threed"
 torrentio_api = f"https://torrentio.strem.fun/providers={providers}|qualityfilter={quality_filters}|limit={limit}"
 
 
 @stream_bp.route('/<user_id>/stream/<content_type>/<content_id>.json')
-async def addon_stream(user_id: str, content_type: str, content_id: str):
+def addon_stream(user_id: str, content_type: str, content_id: str):
     """
     Provide torrentio streams for movie content
     :param user_id: The id of the user requesting the content
@@ -35,6 +37,7 @@ async def addon_stream(user_id: str, content_type: str, content_id: str):
     if content_type not in MANIFEST['types']:
         return respond_with({'streams': [], 'message': 'Content not supported'}, ttl=60)
 
+    get_token(user_id)
     if content_id.startswith(MAL_ID_PREFIX):
         exists, kitsu_id = get_kitsu_id_from_mal_id(content_id)
     elif content_id.startswith('kitsu:'):
@@ -46,13 +49,13 @@ async def addon_stream(user_id: str, content_type: str, content_id: str):
         return respond_with({'streams': [], 'message': 'No Kitsu ID in mapping database'}, ttl=60)
 
     url = f"{torrentio_api}/stream/{content_type}/kitsu:{kitsu_id}.json"
-    resp = await fetch_streams(url)
+    resp = fetch_streams(url)
     if resp.is_error:
         return respond_with({'streams': [], 'message': 'No streams found'}, ttl=60)
     return respond_with(resp.json(), ttl=360)
 
 
-@functools.lru_cache(maxsize=1000)
-async def fetch_streams(url):
-    async with httpx.AsyncClient() as client:
-        return await client.get(url)
+@functools.lru_cache(maxsize=config.STREAM_CACHE_SIZE)
+def fetch_streams(url):
+    with httpx.Client() as client:
+        return client.get(url)
