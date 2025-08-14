478e992b878cbd6f7e1a1dba1119118f98ae3b55
SageTendo <39864316+SageTendo@users.noreply.github.com>
2025-08-14 03:42:06 +0200

    Refactor: code readability Feat: log errors for db queries

--- app/db/db.py
+++ app/db/db.py
@@ -25,7 +25,7 @@ def get_user(user_id: str):
     return UID_map_collection.find_one({"uid": user_id})
 
 
-def store_user(user_details: Dict):
+def store_user(user_details: dict):
     """
     Store user details in db
     :param user_details: The user details to store
@@ -42,7 +42,7 @@ def store_user(user_details: Dict):
 
 
 @lru_cache(maxsize=config.ID_CACHE_SIZE)
-def get_kitsu_id_from_mal_id(mal_id) -> Tuple[bool, str]:
+def get_kitsu_id_from_mal_id(mal_id) -> tuple[bool, str]:
     """
     Get kitsu_id from mal_id from db
     :param mal_id: The MyAnimeList id of the anime
@@ -56,14 +56,14 @@ def get_kitsu_id_from_mal_id(mal_id) -> Tuple[bool, str]:
                 return False, ""
             return True, res["kitsu_id"]
     except KeyError:
-        log_error(f"No Kitsu ID for: MAL:{mal_id}")
+        log_error("KEY ERROR", f"No Kitsu ID for MAL={mal_id}", "Missing mapping in DB")
     except ValueError:
-        log_error(f"Invalid MyAnimeList ID: {mal_id}")
+        log_error("VALUE ERROR", f"Invalid MAL ID: {mal_id}", "Invalid MAL ID")
     return False, ""
 
 
 @lru_cache(maxsize=config.ID_CACHE_SIZE)
-def get_mal_id_from_kitsu_id(kitsu_id) -> Tuple[bool, str]:
+def get_mal_id_from_kitsu_id(kitsu_id) -> tuple[bool, str]:
     """
     Get mal_id from kitsu_id from db
     :param kitsu_id: The kitsu id of the anime
@@ -78,7 +78,9 @@ def get_mal_id_from_kitsu_id(kitsu_id) -> Tuple[bool, str]:
                 return False, ""
             return True, res["mal_id"]
     except KeyError:
-        log_error(f"No MAL ID for KITSU:{kitsu_id}")
+        log_error(
+            "KEY ERROR", f"No MAL ID for Kitsu={kitsu_id}", "Missing mapping in DB"
+        )
     except ValueError:
-        log_error(f"Invalid kitsu ID: {kitsu_id}")
+        log_error("VALUE ERROR", f"Invalid Kitsu ID: {kitsu_id}", "Invalid Kitsu ID")
     return False, ""
--- app/routes/stream.py
+++ app/routes/stream.py
@@ -1,15 +1,15 @@
 import functools
 import urllib.parse
 
-import httpx
+import requests
 from flask import Blueprint
 
 import config
 from app.db.db import get_kitsu_id_from_mal_id
-from app.routes import MAL_ID_PREFIX, IMDB_ID_PREFIX
+from app.routes import IMDB_ID_PREFIX, MAL_ID_PREFIX
 from app.routes.auth import get_valid_user
 from app.routes.manifest import MANIFEST
-from app.routes.utils import respond_with
+from app.routes.utils import handle_api_error, respond_with
 
 stream_bp = Blueprint("stream", __name__)
 
@@ -19,7 +19,7 @@ providers = (
     "torrentgalaxy,magnetdl,horriblesubs,nyaasi,tokyotosho,anidex"
 )
 quality_filters = "3Dbrremux,hdrall,dolbyvision,dolbyvisionwithhdr,threed"
-torrentio_api = f"https://torrentio.strem.fun/providers={providers}|qualityfilter={quality_filters}|limit={limit}"
+TORRENTIO_API = f"https://torrentio.strem.fun/providers={providers}|qualityfilter={quality_filters}|limit={limit}"
 
 
 @stream_bp.route("/<user_id>/stream/<content_type>/<content_id>.json")
@@ -91,14 +91,16 @@ def addon_stream(user_id: str, content_type: str, content_id: str):
             stale_error=invalid_data["staleError"],
         )
 
-    url = f"{torrentio_api}/stream/{content_type}/kitsu:{kitsu_id}.json"
-    resp = fetch_streams(url)
-    if resp.is_error:
+    try:
+        url = f"{TORRENTIO_API}/stream/{content_type}/kitsu:{kitsu_id}.json"
+        resp = fetch_streams(url)
+        resp.raise_for_status()
+        return respond_with(resp.json())
+    except requests.HTTPError as e:
+        handle_api_error(e)
         return respond_with({"streams": [], "message": "Failed to fetch streams"})
-    return respond_with(resp.json())
 
 
 @functools.lru_cache(maxsize=config.STREAM_CACHE_SIZE)
 def fetch_streams(url):
-    with httpx.Client() as client:
-        return client.get(url, timeout=5)
+    return requests.get(url, headers=config.REQ_HEADERS, timeout=10)
