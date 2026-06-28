0ecc23055d76f8fee1b5fddeb4d72bd645dd0837
SageTendo <39864316+SageTendo@users.noreply.github.com>
2026-06-28 17:34:23 +0200

    Fix auth test and add test for user already logged in

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -81,6 +81,8 @@ async def callback():
         _store_user_session(
             {"uid": user_details["id"], "refresh_token": user_details["refresh_token"]}
         )
+        session.pop("code_verifier")
+
         await flash("You are now logged in.", "success")
         return redirect(url_for("ui.index"))
     except requests.HTTPError as e:
@@ -133,5 +135,5 @@ async def logout():
         await flash("You are not logged in.", "warning")
         return redirect(url_for("ui.index"))
 
-    session.pop("user")
+    session.clear()
     return redirect(url_for("ui.index"))
